dbc8ded0c56d7adf26c085064bfbc2ff9f753c73
Mark Corbin <corb0140@algonquinlive.com>
2024-02-03 19:35:28 -0500

    petController setup complete

--- src/routers/petRouter.js
+++ src/routers/petRouter.js
@@ -1,1 +1,20 @@
+"use strict";
 
+const { Router } = require("express");
+const petController = require("../controllers/petController");
+
+const petRouter = Router();
+
+petRouter.post("/", petController.createPet);
+
+petRouter.get("/", petController.getAllPets);
+
+petRouter.get("/:id", petController.getOnePet);
+
+petRouter.put("/:id", petController.replacePet);
+
+petRouter.patch("/:id", petController.updatePet);
+
+petRouter.delete("/:id", petController.deletePet);
+
+module.exports = petRouter;
